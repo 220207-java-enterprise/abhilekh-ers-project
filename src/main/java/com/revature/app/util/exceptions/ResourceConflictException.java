cfa8c18de3fe82d2998905b5d8b3fe941ea78943
abhilekhx <lekh7adhikari@gmail.com>
2022-02-28 01:26:07 -0500

    Added ResourceConflictException

---
+++ src/main/java/com/revature/app/util/exceptions/ResourceConflictException.java
@@ -0,0 +1,7 @@
+package com.revature.app.util.exceptions;
+
+public class ResourceConflictException extends RuntimeException{
+    public ResourceConflictException(String message) {
+        super(message);
+    }
+}
