8f97db93adbcf9942e9ecd7a87e829845fb6f37e
abhilekhx <lekh7adhikari@gmail.com>
2022-03-01 21:13:56 -0500

    worked on ReimbursementServlets post method

---
+++ src/main/java/com/revature/app/servlets/ReimbursementServlet.java
@@ -0,0 +1,61 @@
+package com.revature.app.servlets;
+
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.app.dtos.requests.NewReimbursementRequest;
+import com.revature.app.dtos.responses.ResourceCreationResponse;
+import com.revature.app.models.Reimbursement;
+import com.revature.app.services.ReimbursementService;
+import com.revature.app.services.TokenService;
+import com.revature.app.util.exceptions.InvalidRequestException;
+import com.revature.app.util.exceptions.ResourceConflictException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ReimbursementServlet extends HttpServlet {
+
+    private final TokenService tokenService;
+    private final ReimbursementService reimbursementService;
+    private final ObjectMapper mapper;
+
+    public ReimbursementServlet(TokenService tokenService, ReimbursementService reimbursementService, ObjectMapper mapper) {
+        this.tokenService = tokenService;
+        this.reimbursementService = reimbursementService;
+        this.mapper = mapper;
+    }
+
+    // Get all reimbursements
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    // Create a reimbursement
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter respWriter = resp.getWriter();
+
+        try {
+            // map JSON request
+            NewReimbursementRequest newReimbursementRequest = mapper.readValue(req.getInputStream(),
+                    NewReimbursementRequest.class);
+
+            Reimbursement newReimbursement = reimbursementService.addNewReimbursement(newReimbursementRequest);
+
+            resp.setStatus(201);
+            resp.setContentType("application/json");
+            String payload = mapper.writeValueAsString(new ResourceCreationResponse(newReimbursement.getId()));
+            respWriter.write(payload);
+        } catch (InvalidRequestException | DatabindException e){
+            resp.setStatus(400);
+        } catch (Exception e){
+            resp.setStatus(500);
+        }
+    }
+}
