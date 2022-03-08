f28c2045d11a93253bc5079a9f7e7fb5fe48196e
abhilekhx <lekh7adhikari@gmail.com>
2022-03-07 21:00:19 -0500

    started testing ReimbursementService

--- src/test/java/com/revature/app/services/ReimbursementServiceTest.java
+++ src/test/java/com/revature/app/services/ReimbursementServiceTest.java
@@ -7,7 +7,17 @@ package com.revature.app.services;
 // **************************************************************
 
 import com.revature.app.daos.ReimbursementDAO;
+import com.revature.app.dtos.requests.NewReimbursementRequest;
+import com.revature.app.dtos.responses.ReimbursementResponse;
+import com.revature.app.models.Reimbursement;
+import com.revature.app.util.exceptions.InvalidRequestException;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -24,12 +34,34 @@ public class ReimbursementServiceTest {
     }
 
 
+    @Test(expected = InvalidRequestException.class)
+    public void test_addNewReimbursement_throwsInvalidRequestException_givenNegativeAmount(){
+        // Arrange
+        ReimbursementService spiedSut = Mockito.spy(sut);
+        NewReimbursementRequest invalidReimbursementRequest = new NewReimbursementRequest(-2,"description","1",
+                "LODGING");
+        Reimbursement invalidReimbursement = invalidReimbursementRequest.extractReimbursement();
+
+        try {
+            spiedSut.addNewReimbursement(invalidReimbursementRequest);
+        } finally {
+            verify(mockReimbursementDAO, times(0)).save(invalidReimbursement);
+        }
+    }
+
 //    @Test
-//    public void test_getAllPendingReimbursements_returnsNonNullList(){
-//        List<Reimbursement> allPendingReimbursements = new ArrayList<>();
-//        when(mockReimbursementDAO.getAllPending()).thenReturn(allPendingReimbursements);
-//        List<ReimbursementResponse> result = sut.getAllPendingReimbursements();
-//        Assert.assertNotNull(result);
+//    public void test_addNewReimbursement_returnsReimbursement_givenValidReimbursement(){
+//        ReimbursementService spiedSut = Mockito.spy(sut);
+//        NewReimbursementRequest validReimbursementRequest = new NewReimbursementRequest(100, "description", "1",
+//                "LODGING");
+//        Reimbursement validReimbursement = validReimbursementRequest.extractReimbursement();
+//
+//
+//        doNothing().when(mockReimbursementDAO).save(validReimbursement);
+//
+//        Reimbursement addResult = spiedSut.addNewReimbursement(validReimbursementRequest);
+//
+//        Assert.assertNotNull(addResult);
 //    }
 
 
